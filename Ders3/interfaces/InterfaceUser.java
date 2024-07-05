087337a3c8a2ce7d5777eb3432c03a9a2e1b1bcc
hackerFaruk <onuralparslan97@gmail.com>
2024-07-05 14:15:53 +0300

    interface and classes

---
+++ Ders3/interfaces/InterfaceUser.java
@@ -0,0 +1,35 @@
+
+public class InterfaceUser {
+    public static void main(String[] args) {
+        Teacher mahmut = new Teacher();
+        mahmut.assignHomework();
+    }
+}
+
+// Interface tanımları
+interface Teaching {
+    void teachTopic();
+
+    void assignHomework();
+}
+
+interface Authority {
+    void slinceClass();
+}
+
+class Teacher implements Teaching {
+    public Teacher() {
+        System.out.println("öğretmeye hazırım");
+    }
+
+    @Override
+    public void teachTopic() {
+        System.out.println("calculus türev falan ");
+    }
+
+    @Override
+    public void assignHomework() {
+        System.out.println(" 4 milyon soru çözün");
+
+    }
+}
