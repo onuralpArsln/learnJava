ee4cc802ede84205f0a043e32f3b10d365becfbc
hackerFaruk <onuralparslan97@gmail.com>
2024-07-05 23:39:08 +0300

    using .run for easier runs

---
+++ Ders3/trick/main.java
@@ -0,0 +1,11 @@
+// Unnamed classes and instance main method
+// introduced din java21
+// it is a preview feature that must be enabled 
+// javac --enable-preview Main.java
+// java --enable-preview Main
+
+void main() {
+
+    System.out.println("look ma no classes");
+
+}
