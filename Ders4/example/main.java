dd5dd6469a849cb0adf243946527794a5caf589e
hackerFaruk <onuralparslan97@gmail.com>
2024-07-09 13:21:29 +0300

    açıklma liskov için

--- Ders4/example/main.java
+++ Ders4/example/main.java
@@ -6,6 +6,13 @@ public class main {
 		HumanBase testHuman = new HumanBase("test", 21);
 		HumanPatient testPatient = new HumanPatient("patient", 31, "headache");
 
+		// liskov subs
+		// objects of a superclass should be replaceable
+		// with objects of its subclasses
+		// without affecting the correctness of the program.
+
+		// human base ve human patient methodla düzgn çalışmalı
+
 		Xray testDevice = new Xray();
 		testDevice.imagine(testHuman);
 		System.out.println(testHuman.diagnosis);
