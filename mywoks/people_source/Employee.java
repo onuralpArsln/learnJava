b1679f9f233fb068b0de54bc140d5b25e8ee5748
Almira <miramirza74@gmail.com>
2024-07-20 18:07:59 +0300

    yey

---
+++ mywoks/people_source/Employee.java
@@ -0,0 +1,11 @@
+package people_source;
+
+public class Employee extends humanB{
+    private String task;
+    public Employee(String name, int age, String task){
+        super(name,age);
+        this.task=task;
+    }
+
+}
+        
