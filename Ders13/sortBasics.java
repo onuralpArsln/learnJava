af9a91b8642938bd170fc2a579af5900828ea03b
hackerFaruk <onuralparslan97@gmail.com>
2024-10-04 15:44:46 +0300

    questions

--- Ders13/sortBasics.java
+++ Ders13/sortBasics.java
@@ -1,14 +1,11 @@
-public class sortBasics{
+public class sortBasics {
 
-	public static void main(String[] args){
-		int[] arr = {30,4};
+	public static void main(String[] args) {
+		int[] arr = { 30, 4 };
 		// bunu nasıl sıralarsın?
 
-		
 		// sıralma mantığı 3 elemanla nasıl olmalı?
-		int[] arr = {30,4,23};
+		int[] arr2 = { 30, 4, 23 };
 	}
-	
-
 
 }
