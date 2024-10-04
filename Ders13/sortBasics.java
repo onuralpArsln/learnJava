43f2861051270fba38c60b3cfa4a9a5dab3d2768
hackerFaruk <onuralparslan97@gmail.com>
2024-10-04 15:43:04 +0300

    init

---
+++ Ders13/sortBasics.java
@@ -0,0 +1,14 @@
+public class sortBasics{
+
+	public static void main(String[] args){
+		int[] arr = {30,4};
+		// bunu nasıl sıralarsın?
+
+		
+		// sıralma mantığı 3 elemanla nasıl olmalı?
+		int[] arr = {30,4,23};
+	}
+	
+
+
+}
