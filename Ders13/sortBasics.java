852796b2057327cf662508d2b9b53c6c86e41d58
hackerFaruk <onuralparslan97@gmail.com>
2024-10-04 16:22:17 +0300

    sort start

--- Ders13/sortBasics.java
+++ Ders13/sortBasics.java
@@ -3,9 +3,19 @@ public class sortBasics {
 	public static void main(String[] args) {
 		int[] arr = { 30, 4 };
 		// bunu nasıl sıralarsın?
-
+		sortTwo(arr);
 		// sıralma mantığı 3 elemanla nasıl olmalı?
 		int[] arr2 = { 30, 4, 23 };
+	}
+
+	public static int[] sortTwo(int[] arr) {
+		int[] temparr = new int[arr.length];
+
+		for (int i = 0; i < arr.length; i++) {
+			System.out.println(arr[i]);
+		}
+
+		return temparr;
 	}
 
 }
