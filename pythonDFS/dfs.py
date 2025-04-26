bce0002645f34e1ed9d4f2d21fe47eac64405fb4
onuralpArlsn <onuralparslan97@gmail.com>
2025-04-26 16:31:49 +0300

    added bfs

--- pythonDFS/dfs.py
+++ pythonDFS/dfs.py
@@ -13,7 +13,11 @@ class Travler:
         self.memo=[currentNode]
     
     def roads(self,nodes):
-        return nodes[self.currentNode]  # özel durumları yönet sözlükte key olmayan nodelar var mı mesela h
+        if self.currentNode in nodes:
+            return nodes[self.currentNode]
+        else:
+            return [] 
+        
     
     def walk(self,nodes):
         self.currentNode = self.roads(nodes)[0] 
@@ -36,17 +40,39 @@ class Travler:
             self.memo.append(i)
             visited.append(i)
 
-        if self.dfs(nodes,target,visited): 
-             return self.dfs(nodes,target,visited) #bulduysa sonucu returnle
+            result= self.dfs(nodes,target,visited)   
+            if result: 
+                return result  
         
         self.backtrack()
         return None #hedef bulunamazsa 
+    
+    def bfs(self, nodes, target):
+        queue = [(self.currentNode, [self.currentNode])]  # Kuyruğa başlıyoruz, başlangıç node'u ekleniyor
+        visited = set()  # Ziyaret edilen node'ları tutmak için boş bir set oluşturuyoruz
+        visited.add(self.currentNode)  # Başlangıç node'unu ziyaret edilmiş olarak işaretliyoruz
+        
+        while self.queue_index < len(queue):  # Kuyrukta işlem yapılacak eleman kaldığı sürece döngü devam eder
+            current, path = queue[self.queue_index]  # Kuyruğun başındaki elemanı alıyoruz (current: node, path: o node'a kadar gittiğimiz yol)
+            self.queue_index += 1  # Sıradaki elemanı işlemeye geçiyoruz
+            
+            if current == target:  # Eğer şu anki node hedef node ise
+                return path  # Hedef node'a ulaştık, o zamanki yolu (path) döndürüyoruz
+            
+            # Şu anki node'un komşularını kontrol ediyoruz
+            for neighbor in nodes.get(current, []):  # current node'un komşularını alıyoruz (default boş liste)
+                if neighbor not in visited:  # Eğer komşu daha önce ziyaret edilmediyse
+                    visited.add(neighbor)  # Bu komşuyu ziyaret edilenler listesine ekliyoruz
+                    queue.append((neighbor, path + [neighbor]))  # Komşuyu kuyruğa ekliyoruz ve yolumuza ekliyoruz
+        
+        return None  # Eğer hedef node'a ulaşamamışsak, None döndürüyoruz
+
 
 
 if __name__ == "__main__":
     testTravelar = Travler("a")
     visited=[]
-    target= "b"
+    target= "e"
       
     
     print(testTravelar.roads(nodes))
